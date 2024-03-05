4df3c29552cc7c29ce32fc28c778401d09fe8893
Kat <kathy.uc.17@gmail.com>
2024-03-04 19:16:24 -0500

    Update constant variable

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const URL_BASE = import.meta.env["API_BASE_URL"] + "/api";
+export const URL_BASE = import.meta.env["VITE_API_BASE_URL"] + "/api";
 export const tokenKey = "boardable-auth-token";
